9220376c2563fd0ebc55af548de776f435a247a4
backurdi <bachir.kurdi@gmail.com>
2022-07-19 06:48:40 +0200

    testing cors

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,28 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const PORT = configService.get<number>('PORT');
-  app.enableCors();
+  const whitelist = [
+    'https://sensational-cocada-df9e56.netlify.app/',
+    'https://www.sensational-cocada-df9e56.netlify.app/',
+  ];
+  app.enableCors({
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1) {
+        console.log('allowed cors for:', origin);
+        callback(null, true);
+      } else {
+        console.log('blocked cors for:', origin);
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    allowedHeaders:
+      'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
+    methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
+    credentials: true,
+  });
   await app.listen(PORT || 3000);
 }
 bootstrap();
